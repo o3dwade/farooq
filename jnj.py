e4dac76023316cc1b1f494b82c89e9062b7ec940
Omar Farooq <omarfarooq@Omars-MacBook-Pro.local>
2015-08-17 23:15:24 -0400

    no major modifications

--- jnj.py
+++ jnj.py
@@ -3,20 +3,22 @@ import sys
 
 #script, net_worth, growth, years = argv
 
-net_worth=float(sys.argv[1])
+fcf=float(sys.argv[1])
 growth=float(sys.argv[2])
 years=int(sys.argv[3])
 
-print "The net worth is:", net_worth
+print "The free cash flow is:", fcf
 print "The growth is:", growth
 print "The years of growth is:", years
 
+gp=growth/100
 sum=0
 for x in range (0, years):
-    print "Value for year: ", x, " is ", net_worth
-    net_worth=((net_worth*(growth/100)))+net_worth
+    print "Value for year: ", x, " is ", fcf
+    fcf=((fcf*(gp)))+fcf
+    sum=sum+fcf/((1+gp)**years)
 
-print sum
+print "The PC sum is: ", sum
 
 
     
