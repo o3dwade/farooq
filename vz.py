aa7439c78602d1cb02f22f87009d114fb9e65856
Omar Farooq <ofarooq@ctl-ofarooq-m.local>
2015-08-20 14:10:45 -0400

    Vz push

--- vz.py
+++ vz.py
@@ -11,6 +11,8 @@ print "The free cash flow is:", fcf
 print "The growth is:", growth
 print "The years of growth is:", years
 
+networth=192000
+noshares=4100
 oldfcf=fcf
 gp=growth/100
 sum=float(0)
@@ -18,11 +20,11 @@ for x in range (0, years):
     if x>=10:
         gp = .05
     sum=sum+(fcf/((1+.15)**x))
-    print "Projected FCF for year: ", x+2006, " is ", '{0:.5g}'.format(fcf)#, " PV is: ", '{0:.5g}'.format(sum)
+    print "Projected FCF for year: ", x+2013, " is ", '{0:.5g}'.format(fcf)#, " PV is: ", '{0:.5g}'.format(sum)
     fcf=((fcf*(gp)))+fcf
 
-value=sum-oldfcf+39000
-value=value/2900
+value=sum-oldfcf+networth
+value=value/noshares
 
 print "The value is: ", '{0:.8}'.format(value)
 
