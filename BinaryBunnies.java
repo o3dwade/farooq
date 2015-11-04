027bfc9f25e6eeedd16484eba23fb810964c3191
Omar Farooq <ofarooq@ctl-ofarooq-m.local>
2015-11-04 13:42:21 -0500

    Updated pre post in traversal

--- BinaryBunnies.java
+++ BinaryBunnies.java
@@ -8,9 +8,21 @@ public class BinaryBunnies {
 
 	public static void main(String [] args){
 		BinaryBunnies b = new BinaryBunnies();
+                BinaryBunnies c = new BinaryBunnies();
+                BinaryBunnies d = new BinaryBunnies();
+                BinaryBunnies e = new BinaryBunnies();
+                c.addNode(5);c.addNode(9);c.addNode(8);c.addNode(2);c.addNode(1);
 		b.addNode(5);b.addNode(2);b.addNode(9);b.addNode(1);b.addNode(8);
+                d.addNode(1);d.addNode(2);d.addNode(3);d.addNode(4);d.addNode(5);
+                e.addNode(3);e.addNode(1);e.addNode(4);e.addNode(5);e.addNode(2);
 		b.inOrderTraversal(b.root);
+                c.inOrderTraversal(c.root);
+                d.inOrderTraversal(d.root);
+                e.inOrderTraversal(e.root);
 		System.out.println(b.str);
+                System.out.println(c.str);
+                System.out.println(d.str);
+                System.out.println(e.str);
 	}
 
 	public void addNode(int data){
@@ -47,10 +59,10 @@ public class BinaryBunnies {
 
 	public void inOrderTraversal (Node curr){
 		if (curr!=null){
+                        str = str + curr.data +" ";
 			inOrderTraversal(curr.right);
-			str = str + curr.data +" ";
 			inOrderTraversal(curr.left);
-		}
+                }
 	}
 
     public static String answer(int[] seq) { 
